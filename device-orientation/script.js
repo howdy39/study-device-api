fbf46cca6de45c950fa52261b64ecda0b740406f
howdy39 <tatsuya.howdy@gmail.com>
2018-05-21 22:44:36 +0900

    fix: 画像を追加

--- device-orientation/script.js
+++ device-orientation/script.js
@@ -5,5 +5,14 @@ if (window.DeviceOrientationEvent) {
     const beta = event.beta;
     const gamma = event.gamma;
     console.log("(alpha, beta, gamma):(%s, %s, %s)", alpha, beta, gamma);
+
+    document.querySelector("#howdy39").style.transform =
+      "rotateX(" +
+      (180 + beta) +
+      "deg) rotateY(" +
+      (180 + gamma) +
+      "deg) rotateZ(" +
+      alpha +
+      "deg)";
   });
 }
