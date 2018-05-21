10fac6e5e3e7a2ec4ae43c2cf87fc1f66062af36
howdy39 <tatsuya.howdy@gmail.com>
2018-05-21 22:54:40 +0900

    fix: lotateバグ修正

--- device-orientation/script.js
+++ device-orientation/script.js
@@ -1,18 +1,13 @@
 // Device Orientation API に対応しているかをチェック
 if (window.DeviceOrientationEvent) {
   window.addEventListener("deviceorientation", event => {
-    const alpha = event.alpha;
-    const beta = event.beta;
-    const gamma = event.gamma;
+    const alpha = event.alpha; // 回転軸がZ軸
+    const beta = event.beta; // 回転軸がX軸
+    const gamma = event.gamma; // 回転軸がY軸
     console.log("(alpha, beta, gamma):(%s, %s, %s)", alpha, beta, gamma);
 
-    document.querySelector("#howdy39").style.transform =
-      "rotateX(" +
-      (180 + beta) +
-      "deg) rotateY(" +
-      (180 + gamma) +
-      "deg) rotateZ(" +
-      alpha +
-      "deg)";
+    document.querySelector(
+      "#howdy39"
+    ).style.transform = `rotateX(${beta}deg) rotateY(${gamma}deg) rotateZ(${alpha}deg)`;
   });
 }
