327aec3e0ba1787acd18c8c9b11fec19d5a1099c
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-05-25 09:55:14 +0900

    高精度の有効化

--- geolocation/index.js
+++ geolocation/index.js
@@ -47,7 +47,7 @@ const geoError = error => {
   data.message = errorMessages[error.code];
 };
 const geoOptions = {
-  enableHighAccuracy: false, // 高精度の有効化
+  enableHighAccuracy: true, // 高精度の有効化
   timeout: 5 * 1000, // タイムアウト(ms)
   maximumAge: 10 * 1000 // 位置情報の有効期限(ms)
 };
