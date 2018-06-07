0c9a95b5dcaad5700550e75fd93dc776f91977fd
howdy39 <tatsuya.howdy@gmail.com>
2018-06-07 22:21:58 +0900

    no message

--- device-motion/script.js
+++ device-motion/script.js
@@ -1,19 +1,23 @@
 // Device Motion API に対応しているかをチェック
 if (window.DeviceMotionEvent) {
   window.addEventListener("devicemotion", event => {
-    let x = event.accelerationIncludingGravity.x; // 左右
-    let y = event.accelerationIncludingGravity.y; // 上下
-    let z = event.accelerationIncludingGravity.z; // 前後
-    console.log(x,y,z);
-
-    let log = '(x, y, z):(' + x + ',' + y + ',' + z + ')\n';
-
-    x = parseFloat(event.acceleration.x);
-    y = parseFloat(event.acceleration.y);
-    z = parseFloat(event.acceleration.z);
-    console.log(x,y,z);
-
-    log += '(x, y, z):(' + x + ',' + y + ',' + z + ')'
-    document.querySelector('#log').textContent = log;
+    setTimeout(showLog(event), 1000);
   });
+}
+
+function showLog(event) {
+  let x = event.accelerationIncludingGravity.x; // 左右
+  let y = event.accelerationIncludingGravity.y; // 上下
+  let z = event.accelerationIncludingGravity.z; // 前後
+  console.log(x,y,z);
+
+  let log = '(x, y, z):(' + x + ',' + y + ',' + z + ')\n';
+
+  x = parseFloat(event.acceleration.x);
+  y = parseFloat(event.acceleration.y);
+  z = parseFloat(event.acceleration.z);
+  console.log(x,y,z);
+
+  log += '(x, y, z):(' + x + ',' + y + ',' + z + ')'
+  document.querySelector('#log').textContent = log;
 }
