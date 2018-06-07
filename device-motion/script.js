29a5b76a2426359eca0e0a009bc73c9e3f39c964
howdy39 <tatsuya.howdy@gmail.com>
2018-06-07 23:03:54 +0900

    no message

--- device-motion/script.js
+++ device-motion/script.js
@@ -1,23 +1,19 @@
 // Device Motion API に対応しているかをチェック
 if (window.DeviceMotionEvent) {
   window.addEventListener("devicemotion", event => {
-    setTimeout(showLog(event), 1000);
-  });
-}
+    const x = event.acceleration.x;
+    const y = event.acceleration.y;
+    const z = event.acceleration.z;
+    console.log(x,y,z);
 
-function showLog(event) {
-  const x = event.acceleration.x;
-  const y = event.acceleration.y;
-  const z = event.acceleration.z;
-  console.log(x,y,z);
-
-  const log = '(x, y, z):(\n' + x + ',\n' + y + ',\n' + z + ')';
+    const log = '(x, y, z):(\n' + x + ',\n' + y + ',\n' + z + ')';
     document.querySelector('#log').textContent = log;
 
-  if (Math.abs(x) > 12 || Math.abs(y) > 12 || Math.abs(z) > 12) {
-    const r = Math.random() * 255;
-    const g = Math.random() * 255;
-    const b = Math.random() * 255;
-    document.querySelector('body').style.backgroundColor = `rgba(${r}, ${g}, ${b})`;
-  }
+    if (Math.abs(x) > 12 || Math.abs(y) > 12 || Math.abs(z) > 12) {
+      const r = Math.random() * 255;
+      const g = Math.random() * 255;
+      const b = Math.random() * 255;
+      document.querySelector('body').style.backgroundColor = `rgba(${r}, ${g}, ${b})`;
+    }
+  });
 }
